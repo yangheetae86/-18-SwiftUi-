1d2d67c9d8e02feefc19f8e60643902d1a20c9c1
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-05-26 08:18:08 +0900

    Initial Commit

---
+++ 챕터 18 기본 SwiftUi 프로젝트 분석/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  챕터 18 기본 SwiftUi 프로젝트 분석
+//
+//  Created by HEE TAE YANG on 2020/05/26.
+//  Copyright © 2020 yht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
